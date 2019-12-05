1ea452b2b837cba85a04155fb6b48bca0d14ea06
sunyu1 <sunyu1@green-valley.com>
2019-12-05 14:13:50 +0800

    detail ok

--- src/pages/commodity/commodityAdm/commodityDet.tsx
+++ src/pages/commodity/commodityAdm/commodityDet.tsx
@@ -3,11 +3,13 @@ import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Card, Typography, Alert } from 'antd';
 import styles from './CommodityDet.less';
 import TableList from './component/CommodityDet/Table';
+import RecordList from './component/CommodityDet/Record';
 
 const { Title } = Typography;
 export default (): React.ReactNode => (
   <PageHeaderWrapper className={styles['main']}>
     <Title level={4}>商品名称(SKU编号)</Title>
     <TableList></TableList>
+    <RecordList></RecordList>
   </PageHeaderWrapper>
 );
