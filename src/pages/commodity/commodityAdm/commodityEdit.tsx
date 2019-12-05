c6d8d0cc47d241129d1ad3c0ba10916cd4c19ba0
sunyu1 <sunyu1@green-valley.com>
2019-12-05 14:22:24 +0800

    new tsx

--- src/pages/commodity/commodityAdm/commodityEdit.tsx
+++ src/pages/commodity/commodityAdm/commodityEdit.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Card, Typography, Alert } from 'antd';
 import styles from './CommodityDet.less';
-import TableList from './component/CommodityDet/Table';
 
 const { Title } = Typography;
 export default (): React.ReactNode => (
   <PageHeaderWrapper className={styles['main']}>
-    <Title level={4}>商品编辑</Title>
-    <TableList></TableList>
+    {/* <Title level={4}>商品编辑</Title> */}
   </PageHeaderWrapper>
 );
