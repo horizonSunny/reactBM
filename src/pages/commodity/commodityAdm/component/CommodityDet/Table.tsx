770a661d28c0ae574d7e521fd3e49a712f17b2dd
sunyu1 <sunyu1@green-valley.com>
2019-12-05 09:47:55 +0800

    add

--- src/pages/commodity/commodityAdm/component/CommodityDet/Table.tsx
+++ src/pages/commodity/commodityAdm/component/CommodityDet/Table.tsx
@@ -1,15 +1,52 @@
-import { Table, Divider, Tag, Switch } from 'antd';
+// import { Table, Divider, Tag, Switch } from 'antd';
+
+import { Table } from 'antd';
 import React from 'react';
 import styles from './Table.less';
 
-const pagination = { position: 'bottom', pageSize: '10' };
+const columns = [
+  {
+    dataIndex: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    className: 'column-money',
+    dataIndex: 'money',
+  },
+];
 
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    money: '￥300,000.00',
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    money: '￥1,256,000.00',
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    money: '￥120,000.00',
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
 export default class TableList extends React.Component {
-  state = {
-    pagination,
-  };
+  state = {};
   render() {
     const { state } = this;
-    return <div>123</div>;
+    return (
+      <Table
+        className={styles.main}
+        columns={columns}
+        dataSource={data}
+        bordered
+        pagination={false}
+      />
+    );
   }
 }
