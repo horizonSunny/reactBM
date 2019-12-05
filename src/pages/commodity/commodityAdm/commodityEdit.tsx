8caaf63751a896da819950f373b61c5c240c76dc
sunyu1 <sunyu1@green-valley.com>
2019-12-05 14:35:19 +0800

    add

--- src/pages/commodity/commodityAdm/commodityEdit.tsx
+++ src/pages/commodity/commodityAdm/commodityEdit.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Card, Typography, Alert } from 'antd';
 import styles from './CommodityDet.less';
+import Form from './component/CommodityEdit/Form';
 
 const { Title } = Typography;
 export default (): React.ReactNode => (
   <PageHeaderWrapper className={styles['main']}>
     {/* <Title level={4}>商品编辑</Title> */}
+    <Form></Form>
   </PageHeaderWrapper>
 );
