8677c964e2498e659065b937e0b95016a533271b
sunyu1 <sunyu1@green-valley.com>
2019-12-09 10:50:11 +0800

    ad

--- src/pages/commodity/commodityAdm/CommodityAdm.tsx
+++ src/pages/commodity/commodityAdm/CommodityAdm.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Card, Typography, Alert } from 'antd';
 import styles from './CommodityAdm.less';
+import { connect } from 'dva';
 
 //传入组件
 import SearchForm from './component/CommodityAdm/SearchForm';
 import TableList from './component/CommodityAdm/TableList';
 
-export default (): React.ReactNode => (
-  <PageHeaderWrapper>
-    <SearchForm />
-    <TableList />
-  </PageHeaderWrapper>
-);
+// 请求
+// import { CurrentUser } from '@/models/user';
+// @connect(() => ({}))
+export default class CommodityAdm extends React.Component {
+  // const { dispatch } = this.props;
+  // dispatch({
+  //   type: 'login/logout',
+  // });
+  componentDidMount() {
+    // const { dispatch } = this.props;
+    // dispatch({
+    //   type: 'login/logout',
+    // });
+  }
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <SearchForm />
+        <TableList />
+      </PageHeaderWrapper>
+    );
+  }
+}
