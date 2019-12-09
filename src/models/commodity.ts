f00d05c48b02cba1847c22823da58548a1b8ccf1
sunyu1 <sunyu1@green-valley.com>
2019-12-09 10:50:20 +0800

    ad

---
+++ src/models/commodity.ts
@@ -0,0 +1,29 @@
+import { Effect } from 'dva';
+import { Reducer } from 'redux';
+
+import { productList } from '@/services/commodity';
+
+const CommodityModel = {
+  state: {},
+  namespace: 'commodity',
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      const response = yield call(productList, payload);
+      yield put({
+        type: 'list',
+        payload: response,
+      });
+    },
+  },
+
+  reducers: {
+    list(state, action) {
+      return {
+        ...state,
+        list: action.payload,
+      };
+    },
+  },
+};
+
+export default CommodityModel;
---
+++ src/services/commodity.ts
@@ -0,0 +1,11 @@
+import request from '@/utils/request';
+
+// 获取商品列表信息
+export async function productList(params) {
+  return request('/admin/v1/productList', {
+    method: 'GET',
+    body: {
+      ...params,
+    },
+  });
+}
