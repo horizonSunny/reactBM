4fb966e440f4bdfe2221414b459bd6250bcc5658
sunyu1 <sunyu1@green-valley.com>
2019-12-12 13:55:22 +0800

    over

--- src/pages/commodity/commodityAdm/component/CommodityDet/Record.tsx
+++ src/pages/commodity/commodityAdm/component/CommodityDet/Record.tsx
@@ -4,41 +4,54 @@ import { Table, Carousel } from 'antd';
 import React from 'react';
 import styles from './Table.less';
 import filterData from './filter';
+import { connect } from 'dva';
 
 const columns = [
   {
-    dataIndex: 'operate',
+    dataIndex: 'operation',
   },
   {
     dataIndex: 'operateInfo',
   },
 ];
-const data = [
-  {
-    operate: '下架时间',
-    operateInfo: '2012-03-04 张三',
-  },
-  {
-    operate: '上架时间',
-    operateInfo: '2012-03-04 李四',
-  },
-  {
-    operate: '创建时间',
-    operateInfo: '2012-03-04 王五',
-  },
-];
+// const data = [
+//   {
+//     operation: '下架时间',
+//     optTime: '2012-03-03',
+//     operator: '张三1',
+//   },
+//   {
+//     operation: '上架时间',
+//     optTime: '2012-03-04',
+//     operator: '张三2',
+//   },
+//   {
+//     operation: '创建时间',
+//     optTime: '2012-03-05',
+//     operator: '张三3',
+//   },
+// ];
+@connect(({ commodity }) => ({ commodity }))
 export default class TableList extends React.Component {
   componentDidMount() {}
-
+  state = {
+    data: this.props.commodity.productLog,
+  };
   render() {
     const { state } = this;
+    const dataInfo = state.data.map(item => {
+      return {
+        operation: item['operation'],
+        operateInfo: item['optTime'] + '  ' + item['operator'],
+      };
+    });
     return (
       <div className={styles.main}>
         <a href="javascript:void(0);" className={styles.aInfo}>
           {' '}
           操作记录
         </a>
-        <Table columns={columns} dataSource={data} bordered pagination={false} />
+        <Table columns={columns} dataSource={dataInfo} bordered pagination={false} />
       </div>
     );
   }
