c3a56199f629ee6e319575e8df72ffdee1505400
sunyu1 <sunyu1@green-valley.com>
2020-01-08 17:04:30 +0800

    add

--- src/pages/commodity/commodityClassify/component/commodityCas/casTable.tsx
+++ src/pages/commodity/commodityClassify/component/commodityCas/casTable.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Table, Input, Button } from 'antd';
+import { Table } from 'antd';
 import { DragableBodyRow } from './casTr';
 import { connect } from 'dva';
-import update from 'immutability-helper';
 
 @connect(({ commodityClassify }) => ({
   commodityClassify,
@@ -14,18 +13,9 @@ export default class CasTable extends React.Component {
       row: DragableBodyRow,
     },
   };
-  moveRow = (data, dataName, props, dragIndex, hoverIndex) => {
+  moveRow = (data, props, dragIndex, hoverIndex) => {
     // 这边对拖拽进行一个判断，判断拖拽对物体是不是本classify内的，不是对话直接return
-    console.log('moveRow', dataName, props);
-    const dragRow = data[dragIndex];
-    const obj = new Object();
-    obj[dataName] = {
-      $splice: [
-        [dragIndex, 1],
-        [hoverIndex, 0, dragRow],
-      ],
-    };
-    // this.setState(update(this.state, obj));
+    console.log('moveRow', data, props, dragIndex, hoverIndex);
   };
   render() {
     const columns = [
@@ -35,9 +25,7 @@ export default class CasTable extends React.Component {
       },
     ];
     let dataSourceInfo = this.props.commodityClassify['casInfo' + this.props.levelInfo];
-    console.log('this.props.levelInfo_', this.props.levelInfo);
     return (
-      // <DndProvider backend={HTML5Backend}>
       <div>
         <div className="titleChoose">213123</div>
         <Table
@@ -49,11 +37,10 @@ export default class CasTable extends React.Component {
           components={this.components}
           onRow={(record, index) => ({
             index,
-            moveRow: this.moveRow.bind(this, dataSourceInfo, 'dataOne', record),
+            moveRow: this.moveRow.bind(this, dataSourceInfo, record),
           })}
         />
       </div>
-      // </DndProvider>2
     );
   }
 }
