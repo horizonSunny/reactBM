2a4a166db1d8d9fb1b78b09b03ed79d3c6b7ac31
sunyu1 <sunyu1@green-valley.com>
2020-01-13 10:05:14 +0800

    add

--- src/pages/operTool/findCommodity/findList.tsx
+++ src/pages/operTool/findCommodity/findList.tsx
@@ -3,7 +3,59 @@ import { Table, Button, Divider } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import styles from './findList.less';
 import { connect } from 'dva';
+import dataInfo from '../../../../mock/quick';
 
+const columns = [
+  {
+    title: 'icon',
+    dataIndex: 'image',
+    key: 'quickCategoryId',
+    render: text => <img src={text} />,
+  },
+  {
+    title: '名称',
+    dataIndex: 'categoryName',
+    key: 'categoryName',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: '关联分类',
+    dataIndex: 'categorys',
+    key: 'categorys',
+    render: value => (
+      <a>
+        {value &&
+          value.length !== 0 &&
+          value.map(item => {
+            return item['cateName'] + ',';
+          })}
+      </a>
+    ),
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    key: 'createTime',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    key: 'status',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: '调整排序',
+    dataIndex: 'sort',
+    key: 'sort',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: '操作',
+    dataIndex: 'operate',
+    key: 'operate',
+  },
+];
 @connect(({ commodityClassify }) => ({
   commodityClassify,
 }))
@@ -12,7 +64,7 @@ export default class FindList extends React.Component {
     return (
       <PageHeaderWrapper className={styles['main']}>
         {/* <Title level={4}>商品编辑</Title> */}
-        <div>123</div>
+        <Table columns={columns} dataSource={dataInfo.data.pageList} />
       </PageHeaderWrapper>
     );
   }
