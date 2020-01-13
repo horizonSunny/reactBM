5a831f0c92bb0f74bc5a5b982c8ef82ef50fedb8
sunyu1 <sunyu1@green-valley.com>
2020-01-13 10:20:44 +0800

    add

--- src/pages/operTool/findCommodity/findList.tsx
+++ src/pages/operTool/findCommodity/findList.tsx
@@ -54,13 +54,82 @@ const columns = [
     title: '操作',
     dataIndex: 'operate',
     key: 'operate',
+    render: (text, record) => (
+      <span>
+        <a onClick={this.goToNextPage.bind(this, record, 'detail')}>查看</a>
+        <Divider type="vertical" />
+        <a onClick={this.goToNextPage.bind(this, record, 'editor')}>编辑</a>
+      </span>
+    ),
   },
 ];
 @connect(({ commodityClassify }) => ({
   commodityClassify,
 }))
 export default class FindList extends React.Component {
+  state = {
+    columns: [
+      {
+        title: 'icon',
+        dataIndex: 'image',
+        key: 'quickCategoryId',
+        render: text => <img src={text} />,
+      },
+      {
+        title: '名称',
+        dataIndex: 'categoryName',
+        key: 'categoryName',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: '关联分类',
+        dataIndex: 'categorys',
+        key: 'categorys',
+        render: value => (
+          <a>
+            {value &&
+              value.length !== 0 &&
+              value.map(item => {
+                return item['cateName'] + ',';
+              })}
+          </a>
+        ),
+      },
+      {
+        title: '创建时间',
+        dataIndex: 'createTime',
+        key: 'createTime',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: '状态',
+        dataIndex: 'status',
+        key: 'status',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: '调整排序',
+        dataIndex: 'sort',
+        key: 'sort',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: '操作',
+        dataIndex: 'operate',
+        key: 'operate',
+        render: (text, record) => (
+          <span>
+            <a onClick={this.goToNextPage.bind(this, record, 'detail')}>查看</a>
+            <Divider type="vertical" />
+            <a onClick={this.goToNextPage.bind(this, record, 'editor')}>编辑</a>
+          </span>
+        ),
+      },
+    ],
+  };
+  goToNextPage(record, operate) {}
   render() {
+    const { columns } = this.state;
     return (
       <PageHeaderWrapper className={styles['main']}>
         {/* <Title level={4}>商品编辑</Title> */}
