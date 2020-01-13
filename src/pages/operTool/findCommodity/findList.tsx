5600fc6809f1b492d7ae7c22f48de6bf9c7a695b
sunyu1 <sunyu1@green-valley.com>
2020-01-13 10:52:09 +0800

    add

--- src/pages/operTool/findCommodity/findList.tsx
+++ src/pages/operTool/findCommodity/findList.tsx
@@ -2,67 +2,10 @@ import React from 'react';
 import { Table, Button, Divider } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import styles from './findList.less';
+import router from 'umi/router';
 import { connect } from 'dva';
 import dataInfo from '../../../../mock/quick';
 
-const columns = [
-  {
-    title: 'icon',
-    dataIndex: 'image',
-    key: 'quickCategoryId',
-    render: text => <img src={text} />,
-  },
-  {
-    title: '名称',
-    dataIndex: 'categoryName',
-    key: 'categoryName',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: '关联分类',
-    dataIndex: 'categorys',
-    key: 'categorys',
-    render: value => (
-      <a>
-        {value &&
-          value.length !== 0 &&
-          value.map(item => {
-            return item['cateName'] + ',';
-          })}
-      </a>
-    ),
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'createTime',
-    key: 'createTime',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: '状态',
-    dataIndex: 'status',
-    key: 'status',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: '调整排序',
-    dataIndex: 'sort',
-    key: 'sort',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: '操作',
-    dataIndex: 'operate',
-    key: 'operate',
-    render: (text, record) => (
-      <span>
-        <a onClick={this.editorCategory.bind(this, record)}>编辑</a>
-        <Divider type="vertical" />
-        <a onClick={this.deleteCategory.bind(this, record)}>删除</a>
-      </span>
-    ),
-  },
-];
 @connect(({ commodityClassify }) => ({
   commodityClassify,
 }))
@@ -119,7 +62,7 @@ export default class FindList extends React.Component {
         key: 'operate',
         render: (text, record) => (
           <span>
-            <a onClick={this.editorCategory.bind(this, record)}>编辑</a>
+            <a onClick={this.editorCategory.bind(this)}>编辑</a>
             <Divider type="vertical" />
             <a onClick={this.deleteCategory.bind(this, record)}>删除</a>
           </span>
@@ -128,13 +71,23 @@ export default class FindList extends React.Component {
     ],
   };
   deleteCategory(record) {}
-  editorCategory(record) {}
+  editorCategory(record?) {
+    router.push({
+      pathname: '/operTool/findCommodity/newCategory',
+      // query: { id: params.productId },
+    });
+  }
   render() {
     const { columns } = this.state;
     return (
       <PageHeaderWrapper className={styles['main']}>
         {/* <Title level={4}>商品编辑</Title> */}
-        <Button type="danger" icon="plus-circle" className="buttonAdd">
+        <Button
+          type="danger"
+          icon="plus-circle"
+          className="buttonAdd"
+          onClick={this.editorCategory.bind(this)}
+        >
           添加
         </Button>
         <div
