ca361b4f2e22845f0cc65d8106c2953c7c82b5f6
sunyu1 <sunyu1@green-valley.com>
2020-01-13 09:38:48 +0800

    add

--- src/pages/commodity/commodityClassify/commodityCas.tsx
+++ src/pages/commodity/commodityClassify/commodityCas.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import { DndProvider } from 'react-dnd';
 import { connect } from 'dva';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import HTML5Backend from 'react-dnd-html5-backend';
 // 外部引入
 import styles from './commodityCas.less';
@@ -25,22 +26,24 @@ export default class Table extends React.Component {
   }
   render() {
     return (
-      <DndProvider backend={HTML5Backend}>
-        <Row className={styles['main']}>
-          <Col span={5}>
-            <CasTable levelInfo="One"></CasTable>
-          </Col>
-          <Col span={5}>
-            <CasTable levelInfo="Two"></CasTable>
-          </Col>
-          <Col span={5}>
-            <CasTable levelInfo="Three"></CasTable>
-          </Col>
-          <Col span={9}>
-            <CasCommodity></CasCommodity>
-          </Col>
-        </Row>
-      </DndProvider>
+      <PageHeaderWrapper>
+        <DndProvider backend={HTML5Backend}>
+          <Row className={styles['main']}>
+            <Col span={5}>
+              <CasTable levelInfo="One"></CasTable>
+            </Col>
+            <Col span={5}>
+              <CasTable levelInfo="Two"></CasTable>
+            </Col>
+            <Col span={5}>
+              <CasTable levelInfo="Three"></CasTable>
+            </Col>
+            <Col span={9}>
+              <CasCommodity></CasCommodity>
+            </Col>
+          </Row>
+        </DndProvider>
+      </PageHeaderWrapper>
     );
   }
 }
