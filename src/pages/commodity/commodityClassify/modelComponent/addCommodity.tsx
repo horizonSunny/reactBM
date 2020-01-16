ce946bbae117555b311d465a486adbc227a06195
sunyu1 <sunyu1@green-valley.com>
2020-01-16 16:14:22 +0800

    add

--- src/pages/commodity/commodityClassify/modelComponent/addCommodity.tsx
+++ src/pages/commodity/commodityClassify/modelComponent/addCommodity.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button, Modal, Input } from 'antd';
+import { Button, Modal, Table, Input } from 'antd';
 
 import { connect } from 'dva';
-
+import { commodityItem } from '../component/commodityCas/commodityItem';
+const { Search } = Input;
 @connect(({ commodityClassify }) => ({
   commodityClassify,
 }))
@@ -11,6 +12,11 @@ export default class AddCommodityModal extends React.Component {
     this.props.onRef(this);
   }
   state = { visible: false };
+  components = {
+    body: {
+      row: commodityItem,
+    },
+  };
   showModal = () => {
     this.setState({
       visible: true,
@@ -47,7 +53,62 @@ export default class AddCommodityModal extends React.Component {
       newCateName: value,
     });
   }
+  // 搜索弹框
+  downSelect = () => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div className="buttonContain">
+          <Search
+            style={{
+              marginLeft: '10px',
+              width: '200px',
+            }}
+            placeholder=""
+            onSearch={value => console.log(value)}
+          />
+          <Button
+            type="primary"
+            style={{
+              marginLeft: '20px',
+            }}
+          >
+            添加
+          </Button>
+        </div>
+      </div>
+    );
+  };
+  // 选中添加项
+  onSelectChange = selectedRowKeys => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    // this.setState({ selectedRowKeys });
+    const { dispatch } = this.props;
+    if (dispatch) {
+      dispatch({
+        type: 'commodityClassify/modifyCommodity',
+        payload: selectedRowKeys,
+      });
+    }
+  };
   render() {
+    const columns = [
+      {
+        title: this.downSelect(),
+        dataIndex: 'productCommonName',
+        key: 'productId',
+      },
+    ];
+    // 这里必须用状态管理中的数据,要是this.state会留存上一次的数据
+    const { selectedRowKeys } = this.props.commodityClassify;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
     return (
       <div>
         <Modal
@@ -55,8 +116,18 @@ export default class AddCommodityModal extends React.Component {
           onOk={this.handleOk}
           onCancel={this.handleCancel}
           footer={null}
+          style={{
+            height: '450px',
+          }}
         >
-          <div>123</div>
+          <Table
+            columns={columns}
+            dataSource={this.props.commodityClassify.commodityInfo.pageList}
+            components={this.components}
+            rowSelection={rowSelection}
+            rowKey={record => record.productId}
+            pagination={false}
+          />
         </Modal>
       </div>
     );
