047ff33be3624d53523e1d4a6a0d21870ad22c1d
sunyu1 <sunyu1@green-valley.com>
2020-06-09 17:12:49 +0800

    add

---
+++ src/assets/[1条未读消息][API文档]获取所有产品信息-API-API管理-EOLINKER API管理平台_files/upgrade.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict';
+    window.eoUpgradeInfo={
+        startTime:'2020/4/5 23:30',
+        endTime:'2020/4/5 01:00'
+    }
+})()
---
+++ src/assets/[编辑]产品收藏-API-API管理-EOLINKER API管理平台_files/upgrade.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict';
+    window.eoUpgradeInfo={
+        startTime:'2020/2/20 23:30',
+        endTime:'2020/2/21 01:00'
+    }
+})()
