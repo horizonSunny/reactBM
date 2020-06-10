1c0e4698823b4fe80ff9bfd9bcc1fad55c814ff1
sunyu1 <sunyu1@green-valley.com>
2020-06-10 14:14:20 +0800

    add

---
+++ src/pages/orderMagt/inquiry/components/inquiryDetail/index.tsx
@@ -0,0 +1,6 @@
+import Title from './title';
+import DoctorInfo from './doctorInfo';
+import OrderInfo from './orderInfo';
+import PatientInfo from './patientInfo';
+
+export { Title, DoctorInfo, OrderInfo, PatientInfo };
