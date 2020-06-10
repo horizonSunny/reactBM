a46e8564109803ba28e5d870e9e2b587610f52b1
sunyu1 <sunyu1@green-valley.com>
2020-06-10 15:02:44 +0800

    add

---
+++ config/customPluginConfig.ts
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    'title-font-size': '14px',
+    'title-line-height': '22px',
+    'title-font-weight': 500,
+    'content-font-size': '13px',
+    'content-font-weight': 400,
+    'content-line-height': '21px',
+  },
+};
